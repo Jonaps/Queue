e3d17e3274cf5a5595dc45342d73ea24c963a929
Jonaps <105373463+Jonaps@users.noreply.github.com>
2022-06-06 16:25:56 -0400

    Implementation of Array using Queue

---
+++ ArrayQueue.java
@@ -0,0 +1,58 @@
+// 
+public class ArrayQueue<E> implements Queue<E> {
+        private static final int CAPACITY = 1000;
+        private int f = 0;
+        private int size = 0;
+        private E[] data;
+    
+        public ArrayQueue() {
+            this(CAPACITY);
+        }
+        
+        public ArrayQueue(int capacity) {
+            data = (E[]) new Object[capacity];
+        }
+        
+ 
+    
+    @Override
+    public void enqueue(E e) throws IllegalStateException {
+        if(size() == data.length){
+            throw new IllegalStateException("Queue is full");
+        }
+        int available = (f + size) % data.length;
+        data[available] = e;
+        size++;     
+    }
+
+    @Override
+    public E dequeue() {
+        if(size == 0) {
+            return null;
+        }
+        E answer = data[f];
+        data[f] = null;
+        f = (f + 1) % data.length;
+        size--;
+        return answer;
+    }
+
+    @Override
+    public E first() {
+        if(isEmpty()) {
+            return null;
+        }
+        return data[f];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+    
+}
